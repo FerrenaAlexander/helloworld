7fdab4adb15fd74f9e8fc1f2b07304a8af5f082f
Alex Ferrena <apf2139@columbia.edu>
2018-02-23 16:24:07 -0500

    changed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
     name="helloworld",
     version="0.1",
     packages=find_packages(),
+    author="Alex Ferrena"
+    license="GPLv3",
+    description="A package to say hi, determine the best type of music, calculate the time since the crab nebula supernova, and peace out politely"
     entry_points={
         'console_scripts': ['helloworld = helloworld.__main__:main']
-        }
+        },
+    classifiers=["Programming Language :: Python :: 3"]
 )
